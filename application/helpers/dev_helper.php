de4853e30d12fe664ad4e02c49cba85f8199c252
Awi <102220981+Awi22@users.noreply.github.com>
2024-11-01 13:07:32 +0800

    Update | dev_helper

--- application/helpers/dev_helper.php
+++ application/helpers/dev_helper.php
@@ -3,23 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // fungsi untuk melakukan debug
 if (!function_exists('debug')) {
-    function debug()
-    {
-        $numArgs = func_num_args();
+	function debug()
+	{
+		$numArgs = func_num_args();
 
-        echo 'Total Arguments:' . $numArgs . "\n";
+		echo 'Total Arguments:' . $numArgs . "\n";
 
-        $args = func_get_args();
-        echo '<pre>';
-        foreach ($args as $index => $arg) {
-            echo 'Argument ke-' . $index . ':' . "\n";
-            var_dump($arg);
-            echo "\n";
-            unset($args[$index]);
-        }
-        echo '</pre>';
-        die();
-    }
+		$args = func_get_args();
+		echo '<pre>';
+		foreach ($args as $index => $arg) {
+			echo 'Argument ke-' . $index . ':' . "\n";
+			var_dump($arg);
+			echo "\n";
+			unset($args[$index]);
+		}
+		echo '</pre>';
+		die();
+	}
 }
 
 if (!function_exists('_monthToWeeks')) {
